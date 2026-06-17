822c6a10e1c8be8e6e156b1f5bb6130f46974794
Warren Spence <warrenspence@users.noreply.github.com>
2026-06-18 09:03:55 +1000

    fix(libmaps): fixed title display on nde-location-bottom component
    #18343

--- src/app/libmaps/libmaps-location.component.ts
+++ src/app/libmaps/libmaps-location.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {take} from 'rxjs/operators';
 import {LibmapsBaseComponent} from './libmaps-base.component';
 
 @Component({
@@ -11,5 +12,22 @@ import {LibmapsBaseComponent} from './libmaps-base.component';
 export class LibmapsLocationComponent extends LibmapsBaseComponent {
     protected override get allowedStatuses(): Set<string> {
         return new Set(['available', 'check_holdings']);
+    }
+
+    private titleFromStore: string = '';
+
+    override ngOnInit() {
+        super.ngOnInit();
+
+        // nde-location-bottom hosts don't carry searchResult; pull the title from the NgRx store instead
+        this.hostComponent?.store?.pipe(take(1)).subscribe((state: any) => {
+            const selectedId = state?.['full-display']?.selectedRecordId;
+            const titles = state?.['Search']?.entities?.[selectedId]?.pnx?.display?.title || [];
+            this.titleFromStore = titles.length > 0 ? titles[0] : '';
+        });
+    }
+
+    public override get bookTitle(): string {
+        return this.titleFromStore || '-';
     }
 }
